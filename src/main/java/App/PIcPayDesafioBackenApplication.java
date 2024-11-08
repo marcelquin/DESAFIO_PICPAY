7f7fa9aab398f3c424fb2e0c5d17c6798e7449cd
Marcel Higor quinteiro Araujo <117388829+marcelquin@users.noreply.github.com>
2024-11-08 09:26:28 -0400

    inicio de projeto

---
+++ src/main/java/App/PIcPayDesafioBackenApplication.java
@@ -0,0 +1,13 @@
+package App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PIcPayDesafioBackenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PIcPayDesafioBackenApplication.class, args);
+	}
+
+}
